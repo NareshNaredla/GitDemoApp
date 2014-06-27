f881d0c479b6ef0992d870e8cb0aff08607907d6
naresh <nandu@gmail.com>
2014-06-27 21:44:41 +0530

    Added GitDemoApp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitDemoApp::Application.config.secret_key_base = 'fcdb5d0a427fc309344c7779650ea31bcf21208479733b79736d4895257b683c0aec830c063341df02aa77a6b51143b5d90b7ba740ec020fb1797dedc5a88545'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GitDemoApp::Application.config.session_store :cookie_store, key: '_GitDemoApp_session'
